b4e9d11ae5921949d712e3a616240721363c4d6b
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-07 18:06:08 -0500

    Update 1_Plotting_Demo.py

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -46,9 +46,7 @@ st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
 st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+    """This graph shows distribution of employee pay"""
 )
 
 plotting_demo()
