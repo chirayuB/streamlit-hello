a603c39782707a9143f4f669ba23eecccfff54f3
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-07 20:50:54 -0500

    Add files via upload

---
+++ pages/most_layoff_month.py
@@ -0,0 +1,38 @@
+import time
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+# Placeholder function for data loading
+def load_data(file_path):
+    # Replace this with your actual data loading logic when data is available
+    return pd.DataFrame()
+
+# Function to visualize the data for each scenario
+def plotting_demo(data, title):
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    st.markdown(f"# {title}")
+    st.write(f"This graph shows distribution for {title}")
+    st.button("Re-run")
+
+
+# Page 3: Biggest Decrease Month in Workforce
+st.set_page_config(page_title="Biggest Decrease Month", page_icon="📉")
+st.markdown("# Biggest Decrease Month")
+st.sidebar.header("Biggest Decrease Month")
+plotting_demo(load_data("path/to/your/biggest_decrease_month_data.csv"), "Month with the Biggest Decrease in Workforce")
