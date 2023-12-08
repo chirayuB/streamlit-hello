658f2bf540caf7d84f53e969cec10ece59c6fe83
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-08 00:56:54 -0500

    Update avg_pay_annual.py

--- pages/avg_pay_annual.py
+++ pages/avg_pay_annual.py
@@ -2,19 +2,30 @@ import streamlit as st
 import pandas as pd
 
 def money_paid_chart():
-    st.sidebar.info("This page shows a dynamic chart representing Total Money Paid over Fiscal Years.")
+    st.sidebar.info("This page shows a dynamic chart representing Total Money Paid and Inflation over Fiscal Years.")
     st.sidebar.warning("Note: The data is for demonstration purposes only.")
 
-    # Data Table
-    data = {
+    # Data Table for Total Money Paid
+    data_salary = {
         'Fiscal Year': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
         'Total Money Paid (in Billions)': [22.86, 24.33, 25.52, 27.15, 27.55, 29.52, 30.42, 29.83, 31.74, 31.11]
     }
 
-    df = pd.DataFrame(data)
+    df_salary = pd.DataFrame(data_salary)
 
-    # Chart
-    st.bar_chart(df.set_index('Fiscal Year'))
+    # Data Table for Inflation
+    data_inflation = {
+        'Fiscal Year': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+        'Inflation Rate (%)': [1.6, -0.1, 1.4, 2.5, 2.1, 1.6, 2.5, 4.7, 8.0, 3.2]
+    }
+
+    df_inflation = pd.DataFrame(data_inflation)
+
+    # Merge DataFrames on Fiscal Year
+    df_combined = pd.merge(df_salary, df_inflation, on='Fiscal Year')
+
+    # Bar Chart
+    st.bar_chart(df_combined.set_index('Fiscal Year'))
 
 def main():
     st.set_page_config(page_title="Data Visualization Pages", page_icon="📊")
@@ -23,7 +34,7 @@ def main():
     selected_page = st.sidebar.selectbox("Select a page", ["Total Money Paid Chart"])
 
     if selected_page == "Total Money Paid Chart":
-        st.markdown("# Total Money Paid Chart")
+        st.markdown("# Total Money Paid Chart with Inflation")
         money_paid_chart()
 
 if __name__ == "__main__":
