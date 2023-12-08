7ba39a3f71aa33a722763950dec1d38f83679fe6
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-08 01:54:27 -0500

    Add files via upload

---
+++ pages/start_month.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+def start_month_chart():
+    st.sidebar.info("This page shows a dynamic chart representing the number of employees who started working by month.")
+    st.sidebar.warning("Note: The data is for demonstration purposes only.")
+
+    # Data Table - Start Month
+    data_start_month = {
+        'Start Month': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'Count': [806338, 359621, 326249, 295443, 243674, 291376, 473886, 407760, 1520957, 389203, 262177, 285966]
+    }
+
+    df_start_month = pd.DataFrame(data_start_month)
+
+    # Chart - Bar Chart for Number of Employees by Start Month
+    st.bar_chart(df_start_month.set_index('Start Month'))
+
+def main():
+    st.set_page_config(page_title="Data Visualization Pages", page_icon="📊")
+
+    st.markdown("# Start Month Chart")
+    start_month_chart()
+
+if __name__ == "__main__":
+    main()
