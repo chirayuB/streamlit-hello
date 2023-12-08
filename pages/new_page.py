65c2b707948779fb405eefd395d09d8544ab13d9
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-07 20:16:23 -0500

    Add files via upload

---
+++ pages/new_page.py
@@ -0,0 +1,37 @@
+import time
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+# Placeholder function for data loading
+def load_data(file_path):
+    # Replace this with your actual data loading logic when data is available
+    return pd.DataFrame()
+
+# Function to visualize the data for each scenario
+def plotting_demo(data, title):
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    st.markdown(f"# {title}")
+    st.write(f"This graph shows distribution for {title}")
+    st.button("Re-run")
+
+# Page 1: Number of Staff Employed Compared to Previous Years
+st.set_page_config(page_title="Staff Employed Comparison", page_icon="👥")
+st.markdown("# Staff Employed Comparison")
+st.sidebar.header("Staff Employed Comparison")
+plotting_demo(load_data("path/to/your/staff_employed_data.csv"), "Number of Staff Employed Compared to Previous Years")
