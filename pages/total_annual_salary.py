3dfa8c0558df89da0692b89d5924aa1bbd9a82d0
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 05:46:12 -0500

    Update total_annual_salary.py

--- pages/total_annual_salary.py
+++ pages/total_annual_salary.py
@@ -7,33 +7,27 @@ from streamlit.hello.utils import show_code
 # Placeholder function for data loading
 def load_data(file_path):
     # Replace this with your actual data loading logic when data is available
-    return pd.DataFrame()
+    # For now, we'll return the provided data as a DataFrame
+    data = {
+        'Fiscal Year': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+        'Total Money Paid (in Billions)': [22.86, 24.33, 25.52, 27.15, 27.55, 29.52, 30.42, 29.83, 31.74, 31.11]
+    }
+    return pd.DataFrame(data)
 
-# Function to visualize the data for each scenario
+# Function to visualize the data for the "Total Annual Salary" scenario
 def plotting_demo(data, title):
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+    if data.empty:
+        st.warning(f"No data available for {title}")
+        return
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
-
-    progress_bar.empty()
+    st.bar_chart(data.set_index('Fiscal Year'))
 
     st.markdown(f"# {title}")
-    st.write(f"This graph shows distribution for {title}")
+    st.write(f"This bar chart shows the total money paid (in billions) each fiscal year.")
     st.button("Re-run")
 
-
-    # Page 4: Total Employee Salaries Annually
-    st.set_page_config(page_title="Total Salaries Annually", page_icon="💰")
-    st.markdown("# Total Salaries Annually")
-    st.sidebar.header("Total Salaries Annually")
-    plotting_demo(load_data("path/to/your/total_salaries_annual_data.csv"), "Total Employee Salaries Annually")
-show_code(plotting_demo)
+# Page: Total Annual Salary
+st.set_page_config(page_title="Total Annual Salary", page_icon="💰")
+st.markdown("# Total Annual Salary")
+st.sidebar.header("Total Annual Salary")
+plotting_demo(load_data("path/to/your/total_annual_salary_data.csv"), "Total Annual Salary")
