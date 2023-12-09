231abef0efc2b4f8645a65faadd1df06879957ac
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 10:40:36 -0500

    Update overtime_analysis.py

--- pages/overtime_analysis.py
+++ pages/overtime_analysis.py
@@ -1,5 +1,1 @@
-# Page 10: Overtime Analysis
-st.set_page_config(page_title="Overtime Analysis", page_icon="⏰")
-st.markdown("# Overtime Analysis")
-st.sidebar.header("Overtime Analysis")
-plotting_demo(load_data("path/to/your/overtime_analysis_data.csv"), "Overtime Analysis")
+
