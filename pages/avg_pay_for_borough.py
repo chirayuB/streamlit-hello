6b83dc838d0d978b762629bfacf1d2392d928983
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 11:32:48 -0500

    Update avg_pay_for_borough.py

--- pages/avg_pay_for_borough.py
+++ pages/avg_pay_for_borough.py
@@ -1,34 +1,13 @@
 import streamlit as st
 
-# Placeholder function for data loading
-def load_data(file_path):
-    # Replace this with your actual data loading logic when data is available
-    # For now, we'll return the provided data as a DataFrame
-    data = {
-        'Work Location Borough': ['BRONX'] * 9 + ['BROOKLYN'] * 9 + ['MANHATTAN'] * 9 + ['QUEENS'] * 9,
-        'Fiscal Year': [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023] * 4,
-        'avg_salary': [
-            44626.27237027824, 46477.41344662518, 44402.213804788356, 49584.07435367719, 50989.31965118259,
-            52735.95984245874, 55290.460385715865, 56176.966789944636, 58352.566308159156,
-            48105.01605357891, 50253.5810881394, 49710.4538667813, 52776.89150342302, 53787.940628325996,
-            55156.42707844183, 59288.81568700936, 61179.80880595006, 62048.86133170584,
-            32799.149828488866, 37826.68599229443, 38474.94777980713, 40783.48690581132, 39569.77024472954,
-            42090.37713661531, 43044.49212383632, 42768.40343145001, 46735.63884644754,
-            47721.84717897886, 50649.58732148246, 48482.80001271844, 52137.553875261605, 52340.30731768547,
-            52391.942484636405, 58846.691719808914, 60072.21605640129, 61467.49201467067
-        ]
-    }
-    return pd.DataFrame(data)
-
 # Function to visualize the data for the "Average Salary by Borough" scenario
 def plotting_demo(data, title):
     if data.empty:
         st.warning(f"No data available for {title}")
         return
 
-    # Insert code to generate your chart image and save it
-    # For now, we'll use a placeholder URL
-    chart_image_url = "https://drive.google.com/file/d/1NM8zJpS9eqiUO5ElYLihxReyZ0zvfsRk/view?usp=drive_link"
+    # Replace the placeholder URL with the provided image URL
+    chart_image_url = "https://i.postimg.cc/hvJBWj1V/Screenshot-2023-12-09-at-11-20-20-AM.png"
 
     st.image(chart_image_url, use_container_width=True)
 
