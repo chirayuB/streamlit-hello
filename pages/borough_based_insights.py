9b01a9317e783729ab6ff4ae48400ee7a3eae270
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 10:31:18 -0500

    Update borough_based_insights.py

--- pages/borough_based_insights.py
+++ pages/borough_based_insights.py
@@ -9,25 +9,25 @@ def load_data(file_path):
     # Replace this with your actual data loading logic when data is available
     # For now, we'll return the provided data as a DataFrame
     data = {
-        'Fiscal Year': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
-        'Number of People Left': [43638, 96958, 64033, 55830, 46992, 83606, 94357, 89281, 110725, 70743]
+        'Work Location Borough': ['QUEENS', 'BROOKLYN', 'BRONX', 'MANHATTAN'],
+        'Number of Employees': [562748, 473638, 260086, 3663939]
     }
     return pd.DataFrame(data)
 
-# Function to visualize the data for the "Year-wise Trends" scenario
+# Function to visualize the data for the "Borough-based Insights" scenario
 def plotting_demo(data, title):
     if data.empty:
         st.warning(f"No data available for {title}")
         return
 
-    st.bar_chart(data.set_index('Fiscal Year'))
+    st.bar_chart(data.set_index('Work Location Borough'))
 
     st.markdown(f"# {title}")
-    st.write(f"This bar chart shows the number of people left each fiscal year.")
+    st.write(f"This bar chart shows the number of employees for selected work location boroughs.")
     st.button("Re-run")
 
-# Page 9: Year-wise Trends
-st.set_page_config(page_title="Year-wise Trends", page_icon="📊")
-st.markdown("# Year-wise Trends")
-st.sidebar.header("Year-wise Trends")
-plotting_demo(load_data("path/to/your/year_wise_trends_data.csv"), "Year-wise Trends")
+# Page 8: Borough-based Insights
+st.set_page_config(page_title="Borough-based Insights", page_icon="🏙️")
+st.markdown("# Borough-based Insights")
+st.sidebar.header("Borough-based Insights")
+plotting_demo(load_data("path/to/your/borough_based_insights_data.csv"), "Selected Boroughs Insights")
