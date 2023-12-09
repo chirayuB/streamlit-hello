0f38bbefc8e7257fa4bda1ce4fedbff20cab9511
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 10:18:51 -0500

    Update borough_based_insights.py

--- pages/borough_based_insights.py
+++ pages/borough_based_insights.py
@@ -9,11 +9,8 @@ def load_data(file_path):
     # Replace this with your actual data loading logic when data is available
     # For now, we'll return the provided data as a DataFrame
     data = {
-        'Work Location Borough': ['WASHINGTON DC', 'Unknown', 'QUEENS', 'BROOKLYN', 'WESTCHESTER', 'NASSAU', 'BRONX',
-                                  'RICHMOND', 'SULLIVAN', 'PUTNAM', 'OTHER', 'GREENE', 'SCHOHARIE', 'ORANGE',
-                                  'ULSTER', 'DUTCHESS', 'MANHATTAN', 'DELAWARE', 'ALBANY', 'Queens'],
-        'Number of Employees': [64, 506233, 562088, 473638, 5070, 361, 260086, 69256, 1214, 360, 112598, 89, 262,
-                                25, 2925, 231, 3663939, 802, 143, 660]
+        'Work Location Borough': ['QUEENS', 'BROOKLYN', 'BRONX', 'MANHATTAN'],
+        'Number of Employees': [562748, 473638, 260086, 3663939]
     }
     return pd.DataFrame(data)
 
@@ -26,11 +23,11 @@ def plotting_demo(data, title):
     st.bar_chart(data.set_index('Work Location Borough'))
 
     st.markdown(f"# {title}")
-    st.write(f"This bar chart shows the number of employees for each work location borough.")
+    st.write(f"This bar chart shows the number of employees for selected work location boroughs.")
     st.button("Re-run")
 
 # Page 8: Borough-based Insights
 st.set_page_config(page_title="Borough-based Insights", page_icon="🏙️")
 st.markdown("# Borough-based Insights")
 st.sidebar.header("Borough-based Insights")
-plotting_demo(load_data("path/to/your/borough_based_insights_data.csv"), "Borough-based Insights")
+plotting_demo(load_data("path/to/your/borough_based_insights_data.csv"), "
