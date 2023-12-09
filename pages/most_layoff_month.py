ea2c0584e31212c790486cd9f2fb965375fb115c
chirayuB <152946437+chirayuB@users.noreply.github.com>
2023-12-09 05:36:13 -0500

    Update most_layoff_month.py

--- pages/most_layoff_month.py
+++ pages/most_layoff_month.py
@@ -7,32 +7,27 @@ from streamlit.hello.utils import show_code
 # Placeholder function for data loading
 def load_data(file_path):
     # Replace this with your actual data loading logic when data is available
-    return pd.DataFrame()
+    # For now, we'll return the provided data as a DataFrame
+    data = {
+        'Fiscal Year': [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+        'Number of People Left': [43638, 96958, 64033, 55830, 46992, 83606, 94357, 89281, 110725, 70743]
+    }
+    return pd.DataFrame(data)
 
-# Function to visualize the data for each scenario
+# Function to visualize the data for the "Year-wise Trends" scenario
 def plotting_demo(data, title):
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+    if data.empty:
+        st.warning(f"No data available for {title}")
+        return
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
-
-    progress_bar.empty()
+    st.bar_chart(data.set_index('Fiscal Year'))
 
     st.markdown(f"# {title}")
-    st.write(f"This graph shows distribution for {title}")
+    st.write(f"This bar chart shows the number of people left each fiscal year.")
     st.button("Re-run")
 
-
-# Page 3: Biggest Decrease Month in Workforce
-st.set_page_config(page_title="Biggest Decrease Month", page_icon="📉")
-st.markdown("# Biggest Decrease Month")
-st.sidebar.header("Biggest Decrease Month")
-plotting_demo(load_data("path/to/your/biggest_decrease_month_data.csv"), "Month with the Biggest Decrease in Workforce")
+# Page 9: Year-wise Trends
+st.set_page_config(page_title="Year-wise Trends", page_icon="📊")
+st.markdown("# Year-wise Trends")
+st.sidebar.header("Year-wise Trends")
+plotting_demo(load_data("path/to/your/year_wise_trends_data.csv"), "Year-wise Trends")
